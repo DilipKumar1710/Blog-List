d1b0985ffb09654d5da57388a557d49f44bf35be
DilipKumar <dilipkumar.vaddepally141@gmail.com>
2022-08-06 12:05:31 +0000

    Blog List App

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,16 @@
+import BlogItem from '../BlogItem'
+import './index.css'
+
+const BlogList = props => {
+  const {blogDetails} = props
+
+  return (
+    <ul className="blog-list-container">
+      {blogDetails.map(eachBlog => (
+        <BlogItem key={eachBlog.id} eachBlogDetails={eachBlog} />
+      ))}
+    </ul>
+  )
+}
+
+export default BlogList
